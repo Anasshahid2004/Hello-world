1c07dd9e40f6158c7a46bd3a72685b4fb1f82674
Anas Shahid <114642031+Anasshahid2004@users.noreply.github.com>
2022-10-05 20:40:26 +0500

    Add files via upload
    create hello world

---
+++ index.js
@@ -0,0 +1,8 @@
+// 1.Ways to print in Javascript
+// console.log("Hello world");
+// alert("me");
+// document.write("this is document write")
+
+//2.Javascript console API
+console.log("Hello world", 4 + 6, "Anotherlog");
+console.warn("this is warning")
